5e8598e8f6716f77a0ba6cf2cdec2f1ae81f8cdd
elgomez <elgomez@csumb.edu>
2017-03-02 18:10:55 +0000

    removed old test cases

--- socketio_tests.py
+++ socketio_tests.py
@@ -14,24 +14,25 @@ class SocketIOTestCase(unittest.TestCase):
     #     )
     #     data = from_server['args'][0]
     #     self.assertEquals(data['message'], 'Hey there!')
-    def test_server_relays_message(self):
-        client = app.socketio.test_client(app.app)
-        client.emit('new message', {
-            'my message': 'Potatoes are cool!'
-        })
-        r = client.get_received()
-        print r
-        self.assertEquals(len(r), 2)
-        from_server = r[1]
-        self.assertEquals(
-            from_server['name'],
-            'got your message'
-        )
-        data = from_server['args'][0]
-        self.assertEquals(
-            data['your message'],
-            u'Potatoes are cool!'
-        )
+    # def test_server_relays_message(self):
+    #     client = app.socketio.test_client(app.app)
+    #     client.emit('new message', {
+    #         'my message': 'Potatoes are cool!'
+    #     })
+    #     r = client.get_received()
+    #     print r
+    #     self.assertEquals(len(r), 2)
+    #     from_server = r[1]
+    #     self.assertEquals(
+    #         from_server['name'],
+    #         'got your message'
+    #     )
+    #     data = from_server['args'][0]
+    #     self.assertEquals(
+    #         data['your message'],
+    #         u'Potatoes are cool!'
+    #     )
+    print "testing file"
 
 
 if __name__ == '__main__':
